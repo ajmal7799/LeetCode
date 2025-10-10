c0eb097413465d0fb541a48787cea9f74ee3447d
Ajmal <aju457159@gmail.com>
2025-10-10 16:23:52 +0530

    Time: 1 ms (86.34%), Space: 59 MB (6.98%) - LeetHub

---
+++ 3668-restore-finishing-order/3668-restore-finishing-order.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} order
+ * @param {number[]} friends
+ * @return {number[]}
+ */
+var recoverOrder = function(order, friends) {
+    let result = []
+for(let i=0; i<order.length; i++) {
+    if(friends.includes(order[i])) {
+        result.push(order[i])
+    }
+}
+return result
+};
