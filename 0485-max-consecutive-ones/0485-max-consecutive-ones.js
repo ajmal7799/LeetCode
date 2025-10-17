ed640430d816cde71485667e13162ced992d9cee
Ajmal <aju457159@gmail.com>
2025-10-17 15:45:08 +0530

    Time: 1 ms (92.11%), Space: 61.2 MB (7.39%) - LeetHub

---
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function(nums) {
+    let sa = []
+let count = 0
+for(let i=0; i<nums.length; i++) {
+    if(nums[i]==1){
+        count++
+    }else if(nums[i]==0){
+        sa.push(count)
+        count = 0
+    }
+    
+}
+sa.push(count)
+return Math.max(...sa)
+};
