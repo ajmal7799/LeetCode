88856646e43a5f3e9f8cb5d0e06aafaecb294683
Ajmal <aju457159@gmail.com>
2026-01-19 12:24:01 +0530

    Time: 2 ms (74.25%), Space: 55.5 MB (73.05%) - LeetHub

--- 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.js
+++ 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.js
@@ -4,40 +4,19 @@
  * @return {number}
  */
 var getLucky = function(s, k) {
-    let str = ""
-for(let i=0; i<s.length; i++) {
-    str += (s.charCodeAt(i)-96).toString()
+  let num = "";
+
+for (let ch of s) {
+    num += ch.charCodeAt(0) - 96;
 }
 
-let num = str.split("")
-
-let a = num.map((x)=> Number(x))
-
-let sum = 0
-let count = 0
-for(let i=0; i<1; i++) {
-    sum += a.reduce((acc,curr)=> acc+curr, 0)
-    count++
-}
-
-
-if(count==k) {
-    return sum
-}
-let x= 0
-for(let i=0; i<k-1; i++) {
-    let b = sum.toString().split("")
-    let d = b.map((x)=> Number(x))
-    x = d.reduce((acc,curr)=> acc+curr, 0)
-    count++
-    b=""
-    d=0
-    sum = x
-    if(count==k){
-        break
+for(let i=0; i<k; i++) {
+    let sum = 0
+    for(let digit of num) {
+        sum += Number(digit)
     }
-    
+    num = sum.toString()
 }
 
-return x
+return Number(num)
 };
